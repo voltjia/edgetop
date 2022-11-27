1ab91e03ab6de39f08e038e206a35f1bf7b82d76
Jiacheng Huang <huangjiacheng0709@outlook.com>
2022-11-27 13:00:56 -0500

    Add default edge port and socketing buffer size

--- edge/common.py
+++ edge/common.py
@@ -1,1 +1,3 @@
-cloud_url = 'http://127.0.0.1:5000' # TODO: Use public IP address of a server instead of localhost
+CLOUD_URL = 'http://127.0.0.1:5000' # TODO: Use public IP address of a server instead of localhost
+EDGE_PORT = 5900
+BUFSIZE = 1024
