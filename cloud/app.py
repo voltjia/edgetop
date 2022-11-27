2511a7fab40e038f87cae926170cb226f93c4fc0
Jiacheng Huang <huangjiacheng0709@outlook.com>
2022-11-27 13:00:02 -0500

    Add basic authentication views

--- cloud/app.py
+++ cloud/app.py
@@ -5,6 +5,28 @@ from flask import request
 app = Flask(__name__)
 
 edge_info = {} # TODO: Use a cache or database instead of a built-in dictionary
+user_info = {} # TODO: Use a cache or database instead of a built-in dictionary
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form['username']
+    password = request.form['password']
+    if username in user_info:
+        return 'Username already taken'
+    user_info[username] = password
+    return 'OK'
+
+
+@app.route('/verify_user', methods=['POST'])
+def verify_user():
+    username = request.form['username']
+    password = request.form['password']
+    if username not in user_info:
+        return 'Invalid username'
+    if password != user_info[username]:
+        return 'Invalid password'
+    return 'OK'
 
 
 @app.route('/get_edge_addr', methods=['POST'])
@@ -16,7 +38,5 @@ def get_edge_addr():
 
 @app.route('/report_edge_info', methods=['POST'])
 def report_edge_info():
-    if 'gateway_addr' not in request.form or 'ip_addr' not in request.form:
-        return ''
     edge_info[request.form['gateway_addr']] = request.form['ip_addr']
     return 'OK'
