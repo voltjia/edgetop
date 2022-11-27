ee96f309ebec76a35e1c0a922147fc3433126069
Jiacheng Huang <huangjiacheng0709@outlook.com>
2022-11-27 17:07:45 -0500

    Use Linux's built-in password database to manage users

--- cloud/app.py
+++ cloud/app.py
@@ -1,3 +1,6 @@
+import crypt
+from hmac import compare_digest as compare_hash
+import subprocess
 from flask import Flask
 from flask import request
 
@@ -5,16 +8,43 @@ from flask import request
 app = Flask(__name__)
 
 edge_info = {} # TODO: Use a cache or database instead of a built-in dictionary
-user_info = {} # TODO: Use a cache or database instead of a built-in dictionary
+
+
+def username_exists(username):
+    result = subprocess.run(['getent', 'passwd', username],
+                            capture_output=True)
+    output = result.stdout.decode()
+    # TODO: Handle exceptions
+    return output != ''
+
+
+def password_matches(username, password):
+    result = subprocess.run(f'cat /etc/shadow | grep {username}',
+                            shell=True,
+                            capture_output=True)
+    output = result.stdout.decode()
+    # TODO: Handle exceptions
+    encrypted_password = output.split(':')[1]
+    if not encrypted_password:
+        return True
+    if encrypted_password == 'x' or encrypted_password == '*':
+        raise ValueError('No support for shadow passwords')
+    return compare_hash(crypt.crypt(password, encrypted_password),
+                        encrypted_password)
 
 
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form['username']
     password = request.form['password']
-    if username in user_info:
+    pin = request.form['pin']
+    if username_exists(username):
         return 'Username already taken'
-    user_info[username] = password
+    subprocess.run(['useradd', '-m', username])
+    subprocess.run(f'echo {username}:{password} | chpasswd', shell=True)
+    subprocess.run(
+        f'su {username} -c "printf \'{password}\\n{pin}\\n\\n\' | vncpasswd"',
+        shell=True)
     return 'OK'
 
 
@@ -22,10 +52,10 @@ def register():
 def verify_user():
     username = request.form['username']
     password = request.form['password']
-    if username not in user_info:
-        return 'Invalid username'
-    if password != user_info[username]:
-        return 'Invalid password'
+    if not username_exists(username):
+        return 'Username does not exist'
+    if not password_matches(username, password):
+        return 'Password does not match'
     return 'OK'
 
 
