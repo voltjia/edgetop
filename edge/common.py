0b2ecd2d0e65360e1ad1332736afab22767974fe
Jiacheng Huang <huangjiacheng0709@outlook.com>
2022-11-28 00:44:12 -0500

    Use cloud.edgetop.net instead of localhost

--- edge/common.py
+++ edge/common.py
@@ -1,5 +1,5 @@
-CLOUD_IP = '127.0.0.1' # TODO: Use public IP address of a server instead of localhost
+CLOUD_HOST = 'cloud.edgetop.net'
 CLOUD_PORT = 5000
-CLOUD_URL = f'http://{CLOUD_IP}:{CLOUD_PORT}'
+CLOUD_URL = f'http://{CLOUD_HOST}:{CLOUD_PORT}'
 EDGE_PORT = 5900
 BUFSIZE = 1024
