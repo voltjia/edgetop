aa1b1767a4c236732c3ee1469952f1a3f345bbbb
Jiacheng Huang <huangjiacheng0709@outlook.com>
2022-12-01 13:22:08 -0500

    Add helper function to list members of a given group

--- edge/utility.py
+++ edge/utility.py
@@ -1,3 +1,4 @@
+import re
 import subprocess
 
 
@@ -13,6 +14,16 @@ def get_ip_addr():
     output = result.stdout.decode()
     # TODO: Handle exceptions
     return output.split('src')[-1].split()[0]
+
+
+def list_members_of_group(group_name):
+    result = subprocess.run(['lid', '-g', group_name], capture_output=True)
+    output = result.stdout.decode()
+    splitted = re.split(r'\(([^()]+)\)\n', output)
+    members = []
+    for i in range(0, len(splitted) - 1, 2):
+        members.append(splitted[i].strip())
+    return members
 
 
 if __name__ == '__main__':
